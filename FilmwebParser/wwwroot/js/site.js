e37b5aefa6ec07779a304b3843cd1ebf0bc00a61
DamianOS.MP5 <DamianOS.MP5@gmail.com>
2017-10-17 13:32:18 +0200

    - fix design (add Bootstrap, Font Awesome and Bootswatch to project)

--- FilmwebParser/wwwroot/js/site.js
+++ FilmwebParser/wwwroot/js/site.js
@@ -1,11 +1,14 @@
 ﻿(function () {
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Pokaż panel");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         } else {
-            $(this).text("Ukryj panel");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
     });
 })();
